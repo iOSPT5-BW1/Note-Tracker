6c82273f6615dbcd7d63997c3e17865fc0e2b942
hai666l <0xconr@protonmail.com>
2020-02-26 21:09:34 -0600

    Added note object

---
+++ CreativeNotes/CreativeNotes/note.swift
@@ -0,0 +1,14 @@
+//
+//  note.swift
+//  CreativeNotes
+//
+//  Created by conner on 2/26/20.
+//  Copyright © 2020 conner. All rights reserved.
+//
+
+import Foundation
+
+struct note {
+    var title: String
+    var content: String? // optional because we can have notes without any content
+}
