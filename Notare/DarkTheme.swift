9674712b63d252492928a5c700cc1448e6ea3fe1
conner <0xconr@protonmail.com>
2020-03-04 19:36:47 -0600

    Theme Protocol and classes

---
+++ Notare/DarkTheme.swift
@@ -0,0 +1,9 @@
+//
+//  DarkTheme.swift
+//  Notare
+//
+//  Created by conner on 3/4/20.
+//  Copyright © 2020 Young. All rights reserved.
+//
+
+import Foundation
---
+++ Notare/LightTheme.swift
@@ -0,0 +1,9 @@
+//
+//  LightTheme.swift
+//  Notare
+//
+//  Created by conner on 3/4/20.
+//  Copyright © 2020 Young. All rights reserved.
+//
+
+import Foundation
